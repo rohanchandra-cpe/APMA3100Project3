98ddbbf23bfaab672a121f697dc464ac20bf8c9d
Harish Karumuri <harishskarumuri@gmail.com>
2020-04-15 12:32:02 -0400

    update

--- dataanlysis.R
+++ dataanlysis.R
@@ -4,4 +4,14 @@ output <- read_table2("output.txt",
                       col_names = FALSE, 
                       skip = 4, 
                       n_max = 880,
-                      col_names = c("sampleSize", "mean"))
+                      col_names = c('sampleSize', 'm'))
+
+output = output %>% rename(sampleSize = X1,
+                  m = X2)
+
+output %>% 
+  mutate(sampleSize = factor(sampleSize)) %>% 
+  ggplot(aes(sampleSize, m, col = sampleSize)) + geom_point()
+
+output %>% 
+  ggplot(aes(sampleSize, m, col = sampleSize)) + geom_point()
