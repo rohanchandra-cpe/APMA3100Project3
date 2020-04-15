c333f27659c13c3521a6f2d6ace361de00fa4bbb
Rohan Chandra <rc8yw@virginia.edu>
2020-04-15 16:37:51 -0400

    Added function to generate Zn values as per objective 3 step 2.2

--- RandomNumberGenerator.java
+++ RandomNumberGenerator.java
@@ -10,6 +10,13 @@ public class RandomNumberGenerator {
 	static double K = Math.pow(2.0, 17.0);
 	static int i = 0; 
 	static int desiredNumber = 1000000; //To get values 51,52, and 53
+
+	static double tau = 57;
+	static double scaleParameter = 1/tau;
+	static double expectedValue = (1 / scaleParameter) * Math.sqrt(Math.PI / 2);
+	static double variance = (4 - Math.PI) / (2 * Math.pow(scaleParameter, 2));
+	static double standardDeviation = Math.sqrt(variance);
+
 	//static ArrayList<Double> numArray = new ArrayList<>();
 	static ArrayList<Double> modArray = new ArrayList<>();
 	static ArrayList<Double> m10 = new ArrayList<>();
@@ -20,6 +27,15 @@ public class RandomNumberGenerator {
 	static ArrayList<Double> m250 = new ArrayList<>();
 	static ArrayList<Double> m500 = new ArrayList<>();
 	static ArrayList<Double> m1000 = new ArrayList<>();
+
+	static ArrayList<Double> z10 = new ArrayList<>();
+	static ArrayList<Double> z30 = new ArrayList<>();
+	static ArrayList<Double> z50 = new ArrayList<>();
+	static ArrayList<Double> z100 = new ArrayList<>();
+	static ArrayList<Double> z150 = new ArrayList<>();
+	static ArrayList<Double> z250 = new ArrayList<>();
+	static ArrayList<Double> z500 = new ArrayList<>();
+	static ArrayList<Double> z1000 = new ArrayList<>();
 
 	public static void main(String[] args) {
 		//call random num gen
@@ -41,6 +57,16 @@ public class RandomNumberGenerator {
 		m250 = samples110(250);
 		m500 = samples110(500);
 		m1000 = samples110(1000);
+
+		z10 = standardizedSamples110(m10, 10);
+		z30 = standardizedSamples110(m30, 30);
+		z50 = standardizedSamples110(m50, 50);
+		z100 = standardizedSamples110(m100, 100);
+		z150 = standardizedSamples110(m150, 150);
+		z250 = standardizedSamples110(m250, 250);
+		z500 = standardizedSamples110(m500, 500);
+		z1000 = standardizedSamples110(m1000, 1000);
+
 		System.out.println("All values calculated!");
 
 	}
@@ -90,6 +116,19 @@ public class RandomNumberGenerator {
 		}
 		return ret;
 	}
-	
+	/**
+	 * Function to generate an arraylist of 100 standardized values
+	 * @param arg
+	 * @param n
+	 * @return
+	 */
+	public static ArrayList<Double> standardizedSamples110(ArrayList<Double> arg, int n){
+		ArrayList<Double> returnList = new ArrayList<>();
+		for(Double mVal: arg){
+			double toBeInserted = (mVal - expectedValue) / (standardDeviation / Math.sqrt(n));
+			returnList.add(toBeInserted);
+		}
+		return returnList;
+	}
 	
 }
